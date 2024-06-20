c5a54d2de29859e52430baa55303543475b287f7
EDARA AVINASH <127295146+EDARAAVINASH@users.noreply.github.com>
2024-06-20 21:31:52 +0530

    Time: 1 ms (99.92%) | Memory: 45.9 MB (50.02%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int sum=0;
+        for(int n:nums)
+        {
+            sum=sum^n;
+        }
+        return sum;
+    }
+}
