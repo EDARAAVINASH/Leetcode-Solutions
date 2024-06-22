9c69b03ad47f3230aff975a815a2ed35af5ebee0
Edara Avinash <avinashedara199@gmail.com>
2024-06-22 13:51:43 +0530

    Update find-peak-element.java

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -4,20 +4,12 @@ class Solution {
         
         while (start < end) {
             int mid = (start + end) / 2;
-            
-            // Compare mid element with its next element
             if (nums[mid] > nums[mid + 1]) {
-                // We are in the decreasing part of the array
-                // so the peak element is on the left side (including mid)
                 end = mid;
             } else {
-                // We are in the increasing part of the array
-                // so the peak element is on the right side (excluding mid)
                 start = mid + 1;
             }
         }
-        
-        // At the end, start and end will converge to the peak element
         return start;
     }
 }
