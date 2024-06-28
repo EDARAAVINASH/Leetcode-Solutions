8e013aa051c5d95697bbf23437a101b545dabfbf
Edara Avinash <avinashedara199@gmail.com>
2024-06-28 22:17:38 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (33.48%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int n = rows * cols;
-        
-        long sum = 0, sqr = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                sum += grid[i][j];
-                sqr += (long)grid[i][j] * grid[i][j];
+        long n=grid.length*grid[0].length;
+        long sum=0,sqr=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                sum=sum+grid[i][j];
+                sqr=sqr+(long)grid[i][j]*grid[i][j];
             }
         }
-
-        long totsum = (long)n * (n + 1) / 2;
-        long sqrsum = (long)n * (n + 1) * (2 * n + 1) / 6;
-
-        long diff = sum - totsum;
+        long totsum=(n*(n+1))/2;
+        long sqrsum=(n*(n+1)*(2*n+1))/6;
+        long diff=sum-totsum;
         if (diff == 0) {
-            return new int[] {-1, -1};
+            return new int[] {-1, -1}; 
         }
-        
-        long diff1 = (sqr - sqrsum) / diff;
-
-        long repeated = (diff + diff1) / 2;
-        long missing = diff1 - repeated;
-
-        return new int[] {(int)repeated,(int)missing};
+        long diff1=(sqr-sqrsum)/diff;
+        long x=(diff+diff1)/2;
+        long y=diff1-x;
+        return new int[]{(int)x,(int)y};
     }
 }
