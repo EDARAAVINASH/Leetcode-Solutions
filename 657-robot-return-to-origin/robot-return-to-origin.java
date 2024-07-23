357c8032363f0b8979dbc74d51553f8b200db0a1
Edara Avinash <avinashedara199@gmail.com>
2024-07-24 01:01:38 +0530

    Time: 4 ms (96.52%) | Memory: 44.3 MB (71.27%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean judgeCircle(String moves) 
+    {
+        int vertical=0,horizontal=0;
+        for(char c:moves.toCharArray())
+        {
+            switch(c)
+            {
+                case 'U':vertical++;
+                          break;
+                case 'D':vertical--;
+                          break;
+                case 'L':horizontal++;
+                          break;
+                case 'R':horizontal--;
+                          break;
+            }
+        }
+        if(vertical==0 && horizontal==0) return true;
+        return false;
+    }
+}
