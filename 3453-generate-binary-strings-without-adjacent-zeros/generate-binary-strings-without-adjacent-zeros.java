7016f0d9ed152308585fe380e846c8531554ba43
Edara Avinash <avinashedara199@gmail.com>
2024-08-13 00:17:09 +0530

    Time: 4 ms (41.63%) | Memory: 45.7 MB (65.95%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<String> validStrings(int n) {
-        List<String> result = new ArrayList<>();
-        generate("", n, result);
+        List<String> result=new ArrayList<>();
+        generate("",n,result);
         return result;
     }
-
-    private void generate(String s, int n, List<String> result) {
-        if (s.length() == n) {
+    public static void generate(String s,int n,List<String> result)
+    {
+        if (s.length() == n && !s.contains("00")) {
             result.add(s);
             return;
         }
-
-        // If the last character is not '0', we can safely add '0' without creating "00"
-        if (s.isEmpty() || s.charAt(s.length() - 1) != '0') {
+        if(s.isEmpty() || s.charAt(s.length()-1)!='0')
+        {
             generate(s + '0', n, result);
         }
-
-        // Always safe to add '1'
         generate(s + '1', n, result);
     }
 }
