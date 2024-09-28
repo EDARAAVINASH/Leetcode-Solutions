289dd695e8affc3c9b58b4ad9bd4e4741040fdc5
Edara Avinash <avinashedara199@gmail.com>
2024-09-28 20:55:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (8.85%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -11,36 +11,59 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) 
     {
-        if(k==1) return head;
-        List<List<ListNode>> list1=new ArrayList<>();
-        List<ListNode> list2=new ArrayList<>();
+        if(head==null || k<=1)
+        {
+            return head;
+        }
+        ListNode temp=head;
+        ListNode prevlast=null;
+        while(temp!=null)
+        {
+            ListNode kthnode=findkth(temp,k);
+            if(kthnode==null)
+            {
+                if(prevlast!=null)
+                {
+                    prevlast.next=temp;
+                }
+                break;
+            }
+            ListNode after=kthnode.next;
+            kthnode.next=null;
+            reverse(temp);
+            if(temp==head)
+            {
+                head=kthnode;
+            }
+            else
+            {
+                prevlast.next=kthnode;
+            }
+            prevlast=temp;
+            temp=after;
+        }
+        return head;
+    }
+    public static ListNode findkth(ListNode head,int k)
+    {
+        ListNode temp=head;
+        while(temp!=null && --k>0)
+        {
+            temp=temp.next;
+        }
+        return temp;
+    }
+    public static ListNode reverse(ListNode head)
+    {
         ListNode curr=head;
-        ListNode dummy=new ListNode(-1);
-        ListNode dummytail=dummy;
+        ListNode prev=null;
         while(curr!=null)
         {
-            list2.add(curr);
-            if(list2.size()==k)
-            {
-                list1.add(list2);
-                list2=new ArrayList();
-            }
-            curr=curr.next;
+            ListNode aft=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=aft;
         }
-        for(List<ListNode> list3:list1)
-        {
-            for(int i=list3.size()-1;i>=0;i--)
-            {
-                dummy.next=list3.get(i);
-                dummy=dummy.next;
-            }
-        }
-        for(ListNode n:list2)
-        {
-            dummy.next=n;
-            dummy=dummy.next;
-        }
-        dummy.next=null;
-        return dummytail.next;
+        return curr;
     }
 }
